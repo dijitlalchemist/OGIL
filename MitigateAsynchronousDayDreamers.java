09f7c92b702ac33beddfe377978e11b08a303fc6
gⒶry(String[] args) <dijitlalchemist@users.noreply.github.com>
2013-10-13 14:07:27 -0400

    Update and rename MADDAPI.java to MitigateAsynchronousDayDreamers.java

--- MitigateAsynchronousDayDreamers.java
+++ MitigateAsynchronousDayDreamers.java
@@ -1,3 +1,6 @@
+/*MADD API by Gary Wetter
+*/
+
 public interface MitigateAsynchronousDayDreamers extends OperateCivilianDreams{
 
    int persistentDiagnosis(Stage earlyOn, Stage laterOn, Treatment heavilyMedicate);
